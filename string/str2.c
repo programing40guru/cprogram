c1e287f832b3ce1840523dda17cafa7887de1a58
mehul <mehuldabhi00000@gmail.com>
2024-02-15 15:54:36 +0530

    delate p

--- string/str2.c
+++ string/str2.c
@@ -4,20 +4,13 @@
 char *camp(char s1[],char s2[]){
   int i=0;
 	while(s1[i]!='\0'&&s1[i]==s2[i]){
-	 
-	i++;
-
-	}
-	
+	 	i++;}
 	if(s1[i]==s2[i]){
 		printf("your both string are same okayyy\n");
-
-	}
+}
 	else
 	{
-		printf("oh no your both string are diffrent \n");
-
-	}
+		printf("oh no your both string are diffrent \n");	}
 
 
 }
